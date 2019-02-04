516a22472dec3ab1633677413fc580471ec2bb1e
Craig Alford <crglfrd@gmail.com>
2019-02-04 14:27:25 -0800

    uploaded finished files

--- src/javascript.js
+++ src/javascript.js
@@ -2,58 +2,52 @@
 // add window eventListener for Load
 window.addEventListener("load", init);
 
+function saveArray() {
+    var form = document.querySelector("form"),
+        arrayDiv = form.querySelector("#arrayDiv"),
+        inputs = form.getElementsByTagName( "input" ),
+        output = [],
+        i;
+    
+    // load words to array
+    for (i = 0; i < inputs.length; i++) {
+        output.push(inputs[i].value);
+    }  
+    
+    // display array
+    arrayDiv.innerHTML = output.join(", ");
+}
+
+function shuffleArray() {
+    var form = document.querySelector("form"),
+        shuffledArray = new Array(),
+        shuffledArrayDiv = form.querySelector("#shuffledArrayDiv"),   
+        start = form.querySelector("#arrayDiv").innerHTML,
+        startArray = start.split(", "),
+        lngth = startArray.length,
+        i,
+        num,
+        word;
+                                    
+    for (i = 0; i < lngth; i = i + 1) {
+        num = Math.floor(startArray.length * Math.random());
+        word = startArray[num];
+        startArray.splice(num, 1);
+        shuffledArray.push(word);
+    }
+
+    shuffledArrayDiv.innerHTML = shuffledArray.join(", ");   
+}
+
 function init() {
+    var form = document.querySelector("form");
+        
     if (typeof Storage !== "undefined") {
-        var element = document.getElementByID("printArray"),
-            element2 = document.getElementByID("shuffle");
+        var element = form.querySelector("#saveButton"),
+            element2 = form.querySelector("#shuffleButton");
         
         element.addEventListener("click", saveArray);
         element2.addEventListener("click", shuffleArray);
     }
 }
-
-function saveArray() {
-    var one = document.querSelector("#one"),
-        two = document.querSelector("#two"),
-        three = document.querSelector("#three"),
-        four = document.querSelector("#four"),
-        five = document.querSelector("#six"),
-        six = document.querSelector("#six"),
-        seven = document.querSelector("#seven"),
-        eight = document.querSelector("#eight"),
-        nine = document.querSelector("#nine"),
-        arrayDiv = document.querySelector("#array"),
-        output = new Array();
-                                    
-    output.push(one.value);         
-    output.push(two.value);         
-    output.push(three.value);         
-    output.push(four.value);         
-    output.push(five.value);         
-    output.push(six.value);         
-    output.push(seven.value);         
-    output.push(eight.value);         
-    output.push(nine.value);
-    
-    localStorage.set("array", output);
-                                    
-    arrayDiv.innerHTML = output.join();
-}
-
-function shuffleArray() {
-    var shuffle = new Array(),
-        output = localStorage.get("array");
-                                    
-    for (let i = 0; i < shuffle.length; i++) {
-        var rand = new Random(out.length);
-        let num = Math.floor(output.length * rand);
-        let element = output.slice(num, 1);
-        shuffle.push(element);
-    }
-    
-    shuffledArrayDiv = document.querySelector("#selectedArray");
-    shuffledArrayDiv.innerHTML = shuffledArrayDiv.join();   
-}
-
-
 
